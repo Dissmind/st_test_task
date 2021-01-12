3a330c88ab9ed2fed38bc3db99704ba2c2dd92cf
Dissmind <dissmind@gmail.com>
2021-01-12 14:45:29 +0200

    Добавьте файлы проекта.

---
+++ st_test_task/Controllers/PaydayController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace st_test_task.Controllers
+{
+    [Route("api/payday")]
+    [ApiController]
+    public class PaydayController
+    {
+        [HttpGet("getAll")]
+        public IEnumerable GetPaydayAllEmployers(string from, string to)
+        {
+            return new List<string>() { "First", from, to };
+        }
+
+
+        [HttpGet("get")]
+        public IEnumerable GetPaydayEmployer(int id, string from, string to)
+        {
+            return new List<string>() { "Second", from, to };
+        }
+    }
+}
---
+++ st_test_task/Models/EmployersModel.cs
@@ -0,0 +1,10 @@
+﻿namespace st_test_task.Models
+{
+    public class EmployersModel
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Worked_at { get; set; }
+        public string Group { get; set; }
+    }
+}

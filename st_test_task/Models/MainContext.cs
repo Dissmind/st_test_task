2d0172b094849831c4c90eec4ecf3a510bb5eb9e
Dissmind <dissmind@gmail.com>
2021-01-13 04:02:16 +0200

    [ADD]: EF context init

---
+++ st_test_task/Models/MainContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace st_test_task.Models
+{
+    public class MainContext : DbContext
+    {
+        public DbSet<Employers> employers { get; set; }
+        public DbSet<Bosses> bosses { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=systech.db");
+    }
+}

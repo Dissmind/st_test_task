d8f43e558ce9c42a842612b41f5352707e334058
Dissmind <dissmind@gmail.com>
2021-01-14 01:59:39 +0200

    [ADD]: interface init

---
+++ st_test_task/Classes/Payday.cs
@@ -0,0 +1,42 @@
+﻿namespace st_test_task.Classes
+{
+    public class Payday
+    {
+        public class PaydayResponse
+        {
+            public PaydayResponse(int id, double payday)
+            {
+                Id = id;
+                Payday = payday;
+            }
+
+            public int Id { get; set; }
+            public double Payday { get; set; }
+        }
+
+
+        public static PaydayResponse GetPaydayAllEmployers(string from, string to)
+        {
+            
+            return new PaydayResponse(1, 1.1);
+        }
+        
+        
+        public static PaydayResponse GetPaydayEmployer(int id, string from, string to)
+        {
+            return new PaydayResponse(1, 1.1);
+        }
+
+        
+        private static double AddYear(double value, string group, int countMonth)
+        {
+            return value;
+        }
+
+        
+        private static double AddForEmployers(double value, string group, int countEmployers)
+        {
+            return 1.1;
+        }
+    }
+}

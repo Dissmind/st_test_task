d2f8254e004ae93b8bda0618c1b531c47195e4d9
Dissmind <dissmind@gmail.com>
2021-01-13 04:02:58 +0200

    [ADD]: EmployerController init

---
+++ st_test_task/Controllers/EmployerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using st_test_task.Models;
+
+
+namespace st_test_task.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class EmployerController
+    {
+        
+        private readonly MainContext _context;
+        
+        [HttpGet("/employer")]
+        public IEnumerable GetAll()
+        {
+            return new List<string>();
+        }
+
+
+        [HttpGet("/employer/{id}")]
+        public Employers Get(int id)
+        {
+            return new Employers();
+        }
+
+        [HttpPost]
+        [Route("/employer")]
+        public Employers Set()
+        {
+            return new Employers();
+        }
+    }
+}

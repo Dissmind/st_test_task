b345b265e41b4a65af6a87332bcaa6fdbdd0e896
Dissmind <dissmind@gmail.com>
2021-01-13 04:02:40 +0200

    [ADD]: PaydayController init

--- st_test_task/Controllers/PaydayController.cs
+++ st_test_task/Controllers/PaydayController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using st_test_task.Models;
 
 
 namespace st_test_task.Controllers
@@ -15,6 +16,35 @@ namespace st_test_task.Controllers
         [HttpGet("getAll")]
         public IEnumerable GetPaydayAllEmployers(string from, string to)
         {
+            /*
+            
+            {
+                {
+                id: 1,
+                payday: 1001
+                },
+                {
+                id: 2,
+                payday: 1002
+                }
+            }
+
+             */
+
+
+            using (var db = new TestContext())
+            {
+                db.Add(
+                new TestModel
+                    {
+                        Name = "11testName",
+                        Age = 11
+                    }
+                );
+                
+                db.SaveChanges();
+            }
+
             return new List<string>() { "First", from, to };
         }
 
@@ -22,6 +52,13 @@ namespace st_test_task.Controllers
         [HttpGet("get")]
         public IEnumerable GetPaydayEmployer(int id, string from, string to)
         {
+            /*
+             {
+                id: 1,
+                payday: 1000
+             }
+             */
+
             return new List<string>() { "Second", from, to };
         }
     }

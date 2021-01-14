f144d0d2f89c59b4128d85a4d5c634e6f8c41377
Dissmind <dissmind@gmail.com>
2021-01-14 02:42:26 +0200

    init unit test project

---
+++ st_test_task_unit_test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using st_test_task.Classes;
+
+
+namespace st_test_task_unit_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(4, 4);
+        }
+    }
+}

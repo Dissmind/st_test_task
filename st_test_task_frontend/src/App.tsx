0e49966b27be834ac15f73c4c226e16d9cba7006
Dissmind <dissmind@gmail.com>
2021-01-15 15:42:14 +0200

    [ADD]: react-router-dom

--- st_test_task_frontend/src/App.tsx
+++ st_test_task_frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from "react-router-dom";
+import { Payday } from './pages/payday';
+
+// import Payday from './pages/payday'
 
 function App() {
     return (
         <div>
-            Hello world
+            <BrowserRouter>
+                <Switch>
+                    <Route path={"/payday"}>
+                        <Payday />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }

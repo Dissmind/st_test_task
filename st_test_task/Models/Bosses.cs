45bc50b924a9fef8edc413f00a2e2deccf2df4e1
Dissmind <dissmind@gmail.com>
2021-01-18 05:22:19 +0200

    [ADD]: employer CRUD

---
+++ st_test_task/Models/Bosses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace st_test_task.Models
+{
+    public class Bosses
+    {
+        public Bosses(int id, int bossId)
+        {
+            Id = id;
+            BossId = bossId;
+        }
+
+        public int Id { get; set; }
+        public int BossId { get; set; }
+    }
+}

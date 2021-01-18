8a56783a6d1a66f4d98bee5d5c669896bf3ab0fb
Dissmind <dissmind@gmail.com>
2021-01-18 05:21:53 +0200

    [ADD]: getAllEmployersPayday, getEmployersPayday

--- st_test_task/Classes/Payday.cs
+++ st_test_task/Classes/Payday.cs
@@ -1,7 +1,22 @@
-﻿namespace st_test_task.Classes
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using st_test_task.Models;
+using st_test_task.Repository;
+
+
+namespace st_test_task.Classes
 {
     public class Payday
     {
+        private EmployerRepository _repository;
+        
+        
+        public Payday()
+        {
+            _repository = new EmployerRepository();
+        }
+        
         public class PaydayResponse
         {
             public PaydayResponse(int id, double payday)
@@ -10,33 +25,115 @@
                 Payday = payday;
             }
 
+            public PaydayResponse()
+            {
+                
+            }
+
             public int Id { get; set; }
             public double Payday { get; set; }
         }
+        
 
-
-        public static PaydayResponse GetPaydayAllEmployers(string from, string to)
+        private int GetCountYear(string date)
         {
+            return 1;
+        }
+        
+        public double GetPaydayEmployer(double salary, string group, int countYear, List<double> subEmployersSalary)
+        {
+            double result = salary;
+
+            if (countYear > 0)
+            {
+                result = salary + AddYear(salary, group, countYear);
+            }
             
-            return new PaydayResponse(1, 1.1);
-        }
-        
-        
-        public static PaydayResponse GetPaydayEmployer(int id, string from, string to)
-        {
-            return new PaydayResponse(1, 1.1);
-        }
-
-        
-        private static double AddYear(double value, string group, int countMonth)
-        {
-            return value;
+            if (subEmployersSalary.Count >= 1)
+            {
+                result += AddForEmployers(subEmployersSalary, group);
+            }
+            
+            return result;
         }
 
         
-        private static double AddForEmployers(double value, string group, int countEmployers)
+        private double AddYear(double salary, string group, int countYear)
         {
-            return 1.1;
+            int percentAdd = 0;
+            int percentLimit = 0;
+
+            switch (group)
+            {
+                case "employee":
+                    percentAdd = 3;
+                    percentLimit = 30;
+                    break;
+
+                case "manager":
+                    percentAdd = 5;
+                    percentLimit = 40;
+                    break;
+
+                case "salesman":
+                    percentAdd = 1;
+                    percentLimit = 35;
+                    break;
+                
+                default:
+                    // TODO: throw
+                    break;
+            }
+
+            double result = ((salary / 100) * percentAdd) * countYear;
+
+            double limitAdd = (salary / 100) * percentLimit;
+
+            if (result > limitAdd)
+            {
+                result = limitAdd;
+            }
+            
+            return result;
+        }
+
+        
+        private double AddForEmployers(List<double> subEmployersSalary, string group)
+        {
+            double percent = 0;
+            
+            switch (group)
+            {
+                case "employee":
+                    return 0;
+                    break;
+                
+                case "manager":
+                    percent = 0.5;
+                    break;
+
+                case "salesman":
+                    percent = 0.3;
+                    break;
+                
+                default:
+                    // TODO: throw
+                    break;
+            }
+
+            double result = 0;
+            foreach (var i in subEmployersSalary)
+            {
+                result += (i / 100) * percent;
+            }
+            
+            return result;
         }
     }
 }
+
+
+
+
+
+
--- st_test_task/Controllers/PaydayController.cs
+++ st_test_task/Controllers/PaydayController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using st_test_task.Classes;
 using st_test_task.Models;
+using st_test_task.Repository;
 
 
 namespace st_test_task.Controllers
@@ -13,53 +15,91 @@ namespace st_test_task.Controllers
     [ApiController]
     public class PaydayController
     {
-        [HttpGet("getAll")]
-        public IEnumerable GetPaydayAllEmployers(string from, string to)
+        private MainContext _context;
+        private EmployerRepository repository;
+        
+        public PaydayController()
         {
-            /*
-            
-            {
-                {
-                id: 1,
-                payday: 1001
-                },
-                {
-                id: 2,
-                payday: 1002
-                }
-            }
-
-             */
-
-
-            using (var db = new TestContext())
-            {
-                db.Add(
-                new TestModel
-                    {
-                        Name = "11testName",
-                        Age = 11
-                    }
-                );
-                
-                db.SaveChanges();
-            }
-
-            return new List<string>() { "First", from, to };
+            _context = new MainContext();
+            repository = new EmployerRepository();
         }
 
+        public class GetPaydayAllEmployersResponse
+        {
+            public GetPaydayAllEmployersResponse(int id, double payday)
+            {
+                Id = id;
+                Payday = payday;
+            }
+
+            public GetPaydayAllEmployersResponse() { }
+
+            public int Id { get; set; }
+            public double Payday { get; set; }
+        }
+        
+        [HttpGet("getAll")]
+        public List<GetPaydayAllEmployersResponse> GetPaydayAllEmployers(string from, string to)
+        {
+            var result = new List<GetPaydayAllEmployersResponse>();
+
+            var employersId = _context.Employers.Select(x => x.Id);
+            foreach (var i in employersId)
+            {
+                result.Add(new GetPaydayAllEmployersResponse
+                {
+                    Id = i,
+                    Payday = GetPaydayEmployer(i, from, to)
+                });
+            }
+
+            
+            return result;
+        }
+        
 
         [HttpGet("get")]
-        public IEnumerable GetPaydayEmployer(int id, string from, string to)
+        public double GetPaydayEmployer(int id, string from, string to)
         {
-            /*
-             {
-                id: 1,
-                payday: 1000
-             }
-             */
+            var employer = _context.Employers.Find(id);
+            
+            double salary = employer.Salary;
+            string group = employer.Group;
 
-            return new List<string>() { "Second", from, to };
+
+            // TODO: Высчитать стаж
+            int expYear = 0;
+            
+            // TODO: Высчитать промежуток 
+            int countMonth = 1;
+            
+
+
+            // SUB
+            List<double> subEmployersSalary = new List<double>();
+            
+            var subEmployers = _context.Employers
+                .Where(x => x.BossesId == id)
+                .Select(x => x.Id)
+                .ToList();
+
+            foreach (var i in  subEmployers)
+            {
+                if (i == id)
+                {
+                    continue;
+                }
+                
+                subEmployersSalary.Add(
+                    GetPaydayEmployer(i, "", "")
+                    );
+            }
+
+            var paydayHelper = new Payday();
+            double result = paydayHelper
+                .GetPaydayEmployer(salary, group, expYear, subEmployersSalary);
+            
+            return result * countMonth;
         }
     }
 }

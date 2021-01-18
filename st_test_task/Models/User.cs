3b88b72dd7d1beda469d5d35ceddd9a532a70bad
Dissmind <dissmind@gmail.com>
2021-01-19 01:58:24 +0200

    [ADD]: user model

---
+++ st_test_task/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace st_test_task.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int UserId { get; set; }
+        public int Role { get; set; }
+    }
+}

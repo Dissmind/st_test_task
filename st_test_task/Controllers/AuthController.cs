97c3abf496a233c6ddc21c0457de4352a919c8e0
Dissmind <dissmind@gmail.com>
2021-01-19 03:30:33 +0200

    [ADD]: auth controller

---
+++ st_test_task/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using st_test_task.Models;
+
+
+
+namespace st_test_task.Controllers
+{
+    public class AuthController
+    {
+        private readonly MainContext _context;
+        
+        public AuthController()
+        {
+            _context = new MainContext();
+        }
+        
+        [HttpPost("api/auth")]
+        public User Auth(string login, string password)
+        {
+            var user = _context.Users
+                .Where(x => x.Login == login)
+                .FirstOrDefault(x => x.Password == password);
+
+            return user;
+        }
+    }
+}
+
+
+
+
+
+
